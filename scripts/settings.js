6dfd82df8989ac855d411cf69a8db7a2cb3b3328
NickClaas <148551897+NickClass007@users.noreply.github.com>
2025-02-07 11:43:50 +0100

    Update settings.js

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,3 +1,28 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const toggleSwitch = document.querySelector('#themeToggle');
+  const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
+  if (currentTheme) {
+    document.body.classList.add(currentTheme);
+    if (currentTheme === 'light-mode') {
+      toggleSwitch.checked = false;
+    } else {
+      toggleSwitch.checked = true;
+    }
+  }
+
+  toggleSwitch.addEventListener('change', function() {
+    if (this.checked) {
+      document.body.classList.remove('light-mode');
+      localStorage.setItem('theme', 'dark-mode');
+    } else {
+      document.body.classList.add('light-mode');
+      localStorage.setItem('theme', 'light-mode');
+    }
+  });
+});
+
+
 document.getElementById('dictionary-link').addEventListener('click', function() {
     document.getElementById('dictionary-section').style.display = 'block';
     history.pushState(null, '', 'index.html');
