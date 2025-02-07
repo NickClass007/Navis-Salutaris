93bd410edcaa9175404de66f672bbc048db73ae7
NickClaas <148551897+NickClass007@users.noreply.github.com>
2025-02-07 09:54:33 +0100

    Create settings.js

---
+++ scripts/settings.js
@@ -0,0 +1,14 @@
+document.getElementById('dictionary-link').addEventListener('click', function() {
+    document.getElementById('dictionary-section').style.display = 'block';
+    history.pushState(null, '', 'index.html');
+});
+
+document.getElementById('declination-link').addEventListener('click', function() {
+    document.getElementById('declination-section').style.display = 'block';
+    history.pushState(null, '', 'declination.html');
+});
+
+document.getElementById('settings-link').addEventListener('click', function() {
+    document.getElementById('settings-section').style.display = 'block';
+    history.pushState(null, '', 'settings.html');
+});
