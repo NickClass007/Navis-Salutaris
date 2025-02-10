4f84b2e97b2de2747369399e8d1cc53d6b95794c
NickClaas <148551897+NickClass007@users.noreply.github.com>
2025-02-10 08:14:29 +0100

    Update common.js

--- scripts/common.js
+++ scripts/common.js
@@ -1,7 +1,38 @@
-document.getElementById('color-switch').addEventListener('change', function() {
-    if (this.checked) {
-        document.body.classList.add('light-mode');
+document.addEventListener("DOMContentLoaded", function() {
+  const toggleSwitch = document.querySelector('#themeToggle');
+  const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
+  if (currentTheme) {
+    document.body.classList.add(currentTheme);
+    if (currentTheme === 'light-mode') {
+      toggleSwitch.checked = true;
     } else {
-        document.body.classList.remove('light-mode');
+      toggleSwitch.checked = false;
     }
+  }
+
+  toggleSwitch.addEventListener('change', function() {
+    if (this.checked) {
+      document.body.classList.add('light-mode');
+      localStorage.setItem('theme', 'light-mode');
+    } else {
+      document.body.classList.remove('light-mode');
+      localStorage.setItem('theme', 'dark-mode');
+    }
+  });
+});
+
+document.getElementById('dictionary-link').addEventListener('click', function() {
+    document.getElementById('dictionary-section').style.display = 'block';
+    history.pushState(null, '', 'index.html');
+});
+
+document.getElementById('declination-link').addEventListener('click', function() {
+    document.getElementById('declination-section').style.display = 'block';
+    history.pushState(null, '', 'declination.html');
+});
+
+document.getElementById('settings-link').addEventListener('click', function() {
+    document.getElementById('settings-section').style.display = 'block';
+    history.pushState(null, '', 'settings.html');
 });
