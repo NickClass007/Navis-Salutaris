fbb67d99aaf434d8d95089e3bd54e2754f77d31e
NickClaas <148551897+NickClass007@users.noreply.github.com>
2025-03-16 12:28:06 +0100

    Update index1.js

--- scripts/index1.js
+++ scripts/index1.js
@@ -9,7 +9,8 @@ const urls = [
     'https://raw.githubusercontent.com/nickclass007/Navis-Salutaris/main/Woerter/LW8.csv',
     'https://raw.githubusercontent.com/nickclass007/Navis-Salutaris/main/Woerter/LW9.csv',
     'https://raw.githubusercontent.com/nickclass007/Navis-Salutaris/main/Woerter/LW10.csv',
-    'https://raw.githubusercontent.com/nickclass007/Navis-Salutaris/main/Woerter/LW11.csv'
+    'https://raw.githubusercontent.com/nickclass007/Navis-Salutaris/main/Woerter/LW11.csv',
+    'https://raw.githubusercontent.com/nickclass007/Navis-Salutaris/main/Woerter/Caesars_Bellum_Gallicum/Caesars_Bellum_Gallicum_III.csv'
 ];
 
 const books = [
